29146a3d9aae9412307af08866644cd28b39ee07
kraterkraken <robxxiv@gmail.com>
2022-02-02 23:57:06 -0500

    added some code for resizing, which is not yet functional. improved drag calculations.

--- ezwindow.js
+++ ezwindow.js
@@ -17,6 +17,18 @@ class EZWindowManager
         return this.winList.length - 1; // return the index of the newly added window
     }
 
+    getWindowById(id)
+    {
+        for (let i=0; i < this.winList.length; i++)
+        {
+            if (this.winList[i].frame.id == id)
+            {
+                return this.winList[i];
+            }
+        }
+        return null;
+    }
+
     removeWindow(id)
     {
         for (let i=0; i < this.winList.length; i++)
@@ -67,8 +79,6 @@ Options could include things like:
 7) ask window manager to determine where it opens, instead of providing x, y
 8) dragability
 
-
-Possible Enhancement: METHOD TO ADD WIDGETS
 */
 
 
@@ -77,10 +87,13 @@ class EZWindow
 {
     constructor(x, y, height, width, id, title)
     {
-        // windows have a frame, a titlebar, and a content area
+        this.isDirty = false;
+
+        // windows have a frame, a titlebar (with X button to close), and a content area
         this.frame = document.createElement("div");
         this.frame.id = id;
         this.frame.classList.add("ezwindow_frame");
+        this.frame.style.boxSizing = "border-box"; /* makes widths and heights INCLUDE the border */
         this.frame.style.left = x + "px";
         this.frame.style.top = y + "px";
         this.frame.style.height = height + "px";
@@ -104,17 +117,15 @@ class EZWindow
         this.closeButton.innerHTML = "&#10006"; // symbolic X shape for the window close button
         this.titlebar.appendChild(this.closeButton);
         this.closeButton.onclick = function(e){
-            // might want to check if the window is "dirty"
-            // before dismissing the window
-
             // "this" is the X button (aka, closeButton), therefore ...
             let frame = this.parentNode.parentNode;
             let doc = frame.parentNode;
 
-            // remove the window from the manager's list
-            ezWindowManager.removeWindow(frame.id);
+            // prevent close if window dirty
+            let win = ezWindowManager.getWindowById(frame.id);
+            if (win.isDirty return;  // TODO: warning first?
 
-            //remove the window from the document
+            ezWindowManager.removeWindow(frame.id);
             doc.removeChild(frame);
         }
 
@@ -127,6 +138,7 @@ class EZWindow
         this.frame.appendChild(this.titlebar);
         this.frame.appendChild(this.contentArea);
 
+        //this.makeResizeable(this.frame.id);
         this.makeDraggable(this.frame.id, "ezwindow_titlebar", "ezwindow_closebutton");
         ezWindowManager.addWindow(this);
     }
@@ -136,6 +148,117 @@ class EZWindow
         this.contentArea.appendChild(element);
     }
 
+    makeResizeable(id)
+    {
+        var resizee = document.getElementById(id);
+        if (resizee == null) return;
+        /*******************************************************************/
+        resizee.onmouseover = changePointer;
+        resizee.onmousedown = startResize;
+        /*******************************************************************/
+
+        function changePointer(e)
+        {
+            posX
+        }
+        function allowResize(e)
+        {
+            e = e || window.event(); // I have no idea what this does
+            e.preventDefault(); // I have a vague idea what this does
+
+            // if the target is NOT the resizee, bail out because we are hovering
+            // over one of resizee's children
+            console.log("target.id=" + e.target.id);
+            console.log("currenttarget.id=" + e.currentTarget.id);
+            console.log("resizee.id=" + resizee.id);
+            if (e.target.id != resizee.id) return;
+
+            console.log("allowResize");
+            if (isResizing) return;
+
+            var oldMouseDownHandler = this.onmousedown;
+            this.onmousedown = startResize;
+            this.style.cursor = "n-resize"; // test
+        }
+        function disallowResize(e)
+        {
+            e = e || window.event(); // I have no idea what this does
+            e.preventDefault(); // I have a vague idea what this does
+
+            console.log("disallowResize");
+            if (isResizing) return;
+
+            document.onmousemove = null;
+            document.onmouseup = null;
+            this.onmousedown = oldMouseDownHandler;
+        }
+        function startResize(e)
+        {
+            e = e || window.event(); // I have no idea what this does
+            e.preventDefault(); // I have a vague idea what this does
+
+            isResizing = true;
+
+            // remember where in the window we clicked
+            console.log("startResize: left="+resizee.style.left+", top="+resizee.style.top);
+
+            // **DOCUMENT** must start listening to mouse move events
+            // and mouse up events
+            document.onmousemove = resizeIt;
+            document.onmouseup = stopResizing;
+        }
+        function resizeIt(e)
+        {
+            console.log("resizeIt: cursor="+resizee.style.cursor);
+            console.log("resizeIt: e.clientX="+e.clientX);
+            console.log("resizeIt: e.clientY="+e.clientY);
+            console.log("resizeIt: e.movementX="+e.movementX);
+            console.log("resizeIt: e.movementY="+e.movementY);
+            console.log("resizeIt: width="+resizee.style.width);
+            console.log("resizeIt: height="+resizee.style.height);
+            console.log("resizeIt: clientrect.left="+resizee.getBoundingClientRect().left);
+            console.log("resizeIt: clientrect.right="+resizee.getBoundingClientRect().right);
+            console.log("resizeIt: clientrect.top="+resizee.getBoundingClientRect().top);
+            console.log("resizeIt: clientrect.bottom="+resizee.getBoundingClientRect().bottom);
+
+            var old_top = resizee.getBoundingClientRect().top;
+            var old_bottom = resizee.getBoundingClientRect().bottom;
+            var old_height = old_bottom - old_top;
+
+            // based on which cursor we have, start changing the size
+            if (resizee.style.cursor == "n-resize")
+            {
+                resizee.style.top =  (old_top + e.movementY) + "px"
+                resizee.style.height = (old_height - e.movementY) + "px";
+            }
+            // else if (resizee.style.cursor == "s-resize")
+            // {
+            //     // only need to change the height
+            //     resizee.style.height = e.clientY + "px";
+            // }
+            // else if (resizee.style.cursor = "e-resize")
+            // {
+            //     // only need to change width
+            //     resizee.style.width = e.clientX + "px";
+            // }
+            // else if (resizee.style.cursor = "w-resize")
+            // {
+            //     resizee.style.left = e.clientX - clickX + "px";
+            //
+            // }
+        }
+        function stopResizing(e)
+        {
+            e = e || window.event(); // I have no idea what this does
+            e.preventDefault(); // I have a vague idea what this does
+
+            isResizing = false;
+            document.onmousemove = null;
+            document.onmouseup = null;
+            this.onmousedown = oldMouseDownHandler;
+        }
+    }
+
     makeDraggable(id, targetClass, avoidClass)
     {
         // dragee : the thing we want to drag
@@ -143,17 +266,15 @@ class EZWindow
         //               clicked in order to start dragging the dragee
         //               (if clickTarget not specified, the target is
         //                is just the dragee)
-        var dragee = document.getElementById(id);
+        let dragee = document.getElementById(id);
         if (dragee == null) return;
-        var clickTarget = dragee;
+
+        let clickTarget = dragee;
         if (targetClass != null)
         {
             clickTarget = dragee.querySelector("." + targetClass);
             if (clickTarget == null) return;
         }
-
-        var clickX = 0;
-        var clickY = 0;
 
         clickTarget.onmousedown = startDragging;
 
@@ -164,13 +285,7 @@ class EZWindow
 
             if (avoidClass != null && e.target.className == avoidClass) return;
 
-            // remember where in the window we clicked
-            clickX = e.offsetX;
-            clickY = e.offsetY;
-
-            // now that we are moving the window, we need the
-            // **DOCUMENT** to start listening to mouse move events
-            // and mouse up events
+            // **DOCUMENT** must now start handling mouse up/move events
             document.onmousemove = dragIt;
             document.onmouseup = stopDragging;
         }
@@ -181,8 +296,11 @@ class EZWindow
             e.preventDefault(); // I have a vague idea what this does
 
             // move the window to the new location
-            dragee.style.left = e.clientX - clickX + "px";
-            dragee.style.top = e.clientY - clickY + "px";
+            var old_left = dragee.getBoundingClientRect().left;
+            var old_top = dragee.getBoundingClientRect().top;
+            dragee.style.left = (old_left + e.movementX) + "px";
+            dragee.style.top = (old_top + e.movementY) + "px";
+
         }
 
         function stopDragging(e)
